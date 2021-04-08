9490844ec85aef75ed8f8c6b8271c645fddf41b4
Griffin Dube <46583343+gadube@users.noreply.github.com>
2021-04-08 10:48:38 -0400

    Updating HW I/O for cane

--- hwio.py
+++ hwio.py
@@ -1,41 +1,69 @@
-import time
-import gpiozero
+import time 
+import random
+from gpiozero import LED, Button
 
 def get_distance(iteration):
-    if ((iteration % 2) == 0):
-        if ((iteration % 3) == 0):
-            if ((iteration % 4) == 0):
-                return 2.5
-            return 1.5
-        return 0.5
-    return 0
+	return random.random() * 5.0
+	
 
-def turn_on(distance):
-    if (distance > 0 and distance < 1):
-        print("1 0 0") #one motors on
+def turn_on(distance, l1, l2, l3):
+    if (distance < 3 and distance > 2):
+        print("1 0 0 , Dist = ",distance) #one motors on
+        l1.on()
+        l2.off()
+        l3.off()
     elif (distance < 2 and distance > 1):
-        print("1 1 0") #two motors on
-    elif (distance < 3 and distance > 2):
-        print("1 1 1") #all motors on
+        print("1 1 0 , Dist = ",distance) #two motors on
+        l1.on()
+        l2.on()
+        l3.off()
+    elif (distance > 0 and distance < 1):
+        print("1 1 1 , Dist = ",distance) #all motors on
+        l1.on()
+        l2.on()
+        l3.on()
     else:
-        print("0 0 0") #no motors on
+        print("0 0 0 , Dist = ",distance) #no motors on
+        l1.off()
+        l2.off()
+        l3.off()
 
-def button_pressed(button_object):
-    if (button_object % 7 == 0):
-        print("Look at that TV")
+def run_inference(led):
 
-i = 0
+    #PUT INFERENCE CODE HERE
+
+    led.toggle()
+    print("Look at that TV")
+
+    #END INFERENCE CODE
+
+def inference_wrapper(led):
+
+    #WRAPPER NEEDED FOR BUTTON PRESSES
+
+    return lambda: run_inference(led)
+
+#BEGIN SETTING UP HW I/O
+led1 = LED(14)
+led2 = LED(15)
+led3 = LED(18)
+bled = LED(23)
+button = Button(24,pull_up=False)
+#END SETTING UP HW I/O
+
+#MAIN WHILE LOOP
 while (True):
 
-    i = i + 1
-
-    # get realsense distance
+    # TO BE REPLACED BY: get realsense distance
     dist = get_distance(i)
 
-    # turn on motors
-    turn_on(dist)
+    # turn on motors based on distance
+    turn_on(dist,led1,led2,led3)
 
-    if (button_pressed(i)):
-        run_inference()
+    button.when_pressed = inference_wrapper(bled)
 
-    time.sleep(0.5)
+    time.sleep(2) #distance calc runs every 2 seconds 
+                  #but button interrupt still works during 
+                  #sleep (THIS SHOULD BE REMOVED PROBABLY)
+
+#END MAIN WHILE LOOP
